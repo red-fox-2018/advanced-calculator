b1c6d278b03a35ae8e68665756dd950184ca2a2f
annisaayu <annisaayu569@gmail.com>
2018-04-09 12:03:41 +0700

    advanced calculator

--- calculator.js
+++ calculator.js
@@ -2,19 +2,42 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (num = 0) {
+    this.num = num
   }
-  add () {
+
+  add (inputNum) {
+    this.num += inputNum
+    return this
   }
-  substract () {
+
+  substract (inputNum) {
+    this.num -= inputNum
+    return this
   }
-  multiply () {
+
+  multiply (inputNum) {
+    this.num *= inputNum
+    return this
   }
-  divide () {
+
+  divide (inputNum) {
+    this.num /= inputNum
+    return this
   }
+
   square () {
+    this.num = Math.pow(this.num, 2)
+    return this
   }
+
   squareRoot () {
+    this.num = Math.sqrt(this.num)
+    return this
+  }
+
+  result(){
+    return this.num
   }
 }
 
--- main.js
+++ main.js
@@ -3,3 +3,7 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+let calculator = new Calculator(3)
+
+calculator.add(0).multiply(3).squareRoot()
+console.log(calculator.result());
