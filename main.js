130daabe57d33d6134bd7f193104872a87a1a293
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2018-04-09 13:20:22 +0700

    done

--- main.js
+++ main.js
@@ -3,3 +3,9 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+let calculator = new Calculator();
+
+console.log(calculator.init(5).add(10).multiply(10).squareRoot().square().equal());
+calculator.reset();
+console.log(calculator.pi().multiply(7).multiply(7).equal());
+calculator.reset();
