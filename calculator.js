38cbfe493f70564390a74c15a974785578842043
asepridwan1989 <aseprmail@gmail.com>
2018-04-09 13:47:29 +0700

    done

--- calculator.js
+++ calculator.js
@@ -1,20 +1,45 @@
 'use strict'
-
+const pi = 3.14
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (num) {
+        this.num = num
   }
-  add () {
+  add (addBy) {
+    this.num = this.num + addBy
+    return this
   }
-  substract () {
+  substract (subsBy) {
+    this.num = this.num - subsBy
+    return this
   }
-  multiply () {
+  multiply (mulBy) {
+    this.num = this.num * mulBy
+    return this
   }
-  divide () {
+  divide (divBy) {
+    this.num = this.num / divBy
+    return this
   }
-  square () {
+  square (sqBy) {
+    let multiplier = this.num
+    for (let i = 0; i < sqBy-1; i++) {
+        this.num = this.num*multiplier
+    }
+    return this
   }
   squareRoot () {
+      this.num = Math.sqrt(this.num)
+      return this
+  }
+  circleRound(){
+      this.num = 2*pi*this.num
+      return this
+  }
+  circleArea(){
+      let radiusSquare = this.num*this.num
+      this.num = pi*radiusSquare
+      return this
   }
 }
 
