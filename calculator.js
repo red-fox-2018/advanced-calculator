42b83d3daf3a3ab489bfbd25116c5b93abb6273f
AdhityaRdt <rahmanadhitya2201@gmail.com>
2018-04-09 10:47:43 +0700

    anchor telah selesai

--- calculator.js
+++ calculator.js
@@ -2,19 +2,40 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (inputNumber) {
+    this.number = inputNumber
   }
-  add () {
+  add (addNumber) {
+    this.number = this.number + addNumber
+    return this
   }
-  substract () {
+  substract (substractNumber) {
+    this.number = this.number - substractNumber
+    return this
   }
-  multiply () {
+  multiply (multiplyNumber) {
+    this.number = this.number * multiplyNumber
+    return this
   }
-  divide () {
+  divide (divideNumber) {
+    this.number = this.number / divideNumber
+    return this
   }
-  square () {
+  square (powNumber) {
+    this.number = Math.pow(this.number,powNumber)
+    return this
   }
   squareRoot () {
+    this.number = Math.sqrt(this.number)
+    return this
+  }
+  circleArea(){// menghitung luas dimana this.number sebagai radius
+    this.number = 3.14 * Math.pow(this.Number,2)
+    return this
+  }
+  circleRound(){// mengitung keliling dimana this.number sebagai radius
+    this.number = 2 * 3.14 * this.number
+    return this
   }
 }
 
@@ -27,3 +48,5 @@ class Calculator {
 */
 
 module.exports = Calculator
+let calculator = new Calculator(5)
+console.log(calculator.add(10).substract(2).square(2).squareRoot().circleRound().number);
