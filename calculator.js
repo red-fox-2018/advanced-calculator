b3da38780038e5e656f5a6f76f1715dbf9d198ca
violerine <viosefferine@gmail.com>
2018-04-09 19:44:29 +0700

    kalkulator

--- calculator.js
+++ calculator.js
@@ -1,29 +1,49 @@
-'use strict'
+class Calculator{
+    constructor(input){
+        this.input=input
+        this.result = 0
+    }
 
-class Calculator {
-  //write your code here
-  constructor () {
-  }
-  add () {
-  }
-  substract () {
-  }
-  multiply () {
-  }
-  divide () {
-  }
-  square () {
-  }
-  squareRoot () {
-  }
+    //jadi
+    addition(input){
+       this.result= this.result+input
+       return this
+    }
+
+    subtraction(input){
+        this.result=-input
+        return this
+    }
+
+    multiplication(input){
+        this.result*=input
+        return this
+    }
+
+    square(input){
+        this.result=Math.pow(this.result,input)
+        return this
+    }
+
+
+    nthRoot(input){
+        this.result=Math.pow(this.result, 1/input)
+        return this
+    }
+
+    circleArea(){
+       //this.result is the radius
+       this.result=Math.PI * Math.pow(this.result,2)
+        return this
+    }
+    
+    circleCircumferences(){
+        this.result=2*Math.PI*this.result
+        return this
+    }
 }
 
-/** note : you can use several features from ecmascript, such as:
-* - Classes
-* - Default Parameters
-* - Destructured Assignment
-* - Template Literals
-* - Method Chaining
-*/
-
-module.exports = Calculator
+var calcu = new Calculator(5)
+calcu.addition(5)
+console.log(calcu.addition(5).subtraction(7).result)
+// .add(5).multiplication(5).result
